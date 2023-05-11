b18d43f3beeb145f4682df4b418a0f5d681d0f0e
豊見山裕貴 <tomiyamayuki@yutakamiyamayukinoMacBook-Air.local>
2023-05-11 15:44:35 +0900

    ver2

--- test.py
+++ test.py
@@ -2,6 +2,14 @@ import datasets
 X,Y = datasets.load_linear_example1()
 print(X,Y)
 
+#ver1
 import regression
 model = regression.LinearRegression()
 print(model.x)
+
+#ver2
+import importlib
+importlib.reload(regression)
+model = regression.LinearRegression()
+model.fit(X,Y)
+print(model.theta)
